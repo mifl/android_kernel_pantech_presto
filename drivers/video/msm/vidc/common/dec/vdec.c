1732b1d3b342076adbb23ab0e1ab3f2ee3970e56
Deepak Kotur <dkotur@codeaurora.org>
2012-05-04 16:12:45 -0700

    vidc: Flush the cached buffer only if the buffer is valid.
    Before flushing the cached buffer check for the following
    - When buffer allocation fails, do not flush the buffer
      report error and return.
    - Check if the buffer handle is valid, before flushing the
      buffer.
    
    CRs-Fixed: 358873
    Change-Id: I58b869d2d2eb72f991649d62a5186c7b0f5adcf4
    Signed-off-by: Deepak Kotur <dkotur@codeaurora.org>

--- drivers/video/msm/vidc/common/dec/vdec.c
+++ drivers/video/msm/vidc/common/dec/vdec.c
@@ -347,7 +347,7 @@ static void vid_dec_output_frame_done(struct video_client_ctx *client_ctx,
 		ion_flag = vidc_get_fd_info(client_ctx, BUFFER_TYPE_OUTPUT,
 				pmem_fd, kernel_vaddr, buffer_index,
 				&buff_handle);
-		if (ion_flag == ION_FLAG_CACHED) {
+		if (ion_flag == ION_FLAG_CACHED && buff_handle) {
 			msm_ion_do_cache_op(client_ctx->user_ion_client,
 					buff_handle,
 					(unsigned long *) kernel_vaddr,
@@ -1275,7 +1275,7 @@ static u32 vid_dec_decode_frame(struct video_client_ctx *client_ctx,
 						kernel_vaddr,
 						buffer_index,
 						&buff_handle);
-			if (ion_flag == ION_FLAG_CACHED) {
+			if (ion_flag == ION_FLAG_CACHED && buff_handle) {
 				msm_ion_do_cache_op(client_ctx->user_ion_client,
 				buff_handle,
 				(unsigned long *)kernel_vaddr,
