45f867ba7ddfafb37f193598f897236145245cef
Arun Menon <menon@codeaurora.org>
2012-08-23 15:55:38 -0700

    msm: vidc: error handling for ion_map_iommu calls.
    Added a check for non-zero virtual address.
    Without this change, when ion_map_iommu returns a
    zero virtual address, video core continuous ahead
    without error check and later target reset happens
    when trying to do a ion_unmap_iommu on the above fd,
    which was never mapped.
    
    Signed-off-by: Arun Menon <menon@codeaurora.org>
    (cherry picked from commit 8688368ce3afbd1f3a80c7fa688c6a2f467247d3)
    
    Change-Id: Ic6d86cc621e2f524dd19d1677646822cdc0f317c
    Signed-off-by: Sudhir Sharma <sudsha@codeaurora.org>

--- drivers/video/msm/vidc/common/vcd/vcd_sub.c
+++ drivers/video/msm/vidc/common/vcd/vcd_sub.c
@@ -119,8 +119,10 @@ static int vcd_pmem_alloc(size_t sz, u8 **kernel_vaddr, u8 **phy_addr,
 				(unsigned long *)&iova,
 				(unsigned long *)&buffer_size,
 				0, 0);
-			if (ret) {
-				pr_err("%s() ION iommu map failed", __func__);
+			if (ret || !iova) {
+				pr_err(
+				"%s() ION iommu map failed, ret = %d, iova = 0x%lx",
+					__func__, ret, iova);
 				goto ion_map_bailout;
 			}
 			map_buffer->phy_addr = iova;
