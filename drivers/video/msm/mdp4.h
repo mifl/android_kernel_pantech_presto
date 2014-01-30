4ef03598f63bc101761856b86c840d45e4d79c84
MIFL <mifl.pc@gmail.com>
2014-01-30 17:40:21 +0100

    msm_fb: display: provision of mixer reset function
    Mdp has hardware bug which cause fetch engine stalled if less than
    3 pixels at begin or end of line be cropped into destination. Reset
    mixer can fix this problem. However frame work can provide better
    work around solution by falling back to gpu if the condition detected.
    This patch will not reset mixer but provide mixer reset function in
    case of mixer reset is need in future.
    
    CRs-fixed: 443018
    Change-Id: I38ee591f28bf1b81f4fbd58838281b43ba0a8498
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -798,6 +798,7 @@ void mdp4_primary_vsync_dsi_video(void);
 void mdp4_dsi_cmd_base_swap(int cndx, struct mdp4_overlay_pipe *pipe);
 void mdp4_dsi_cmd_wait4vsync(int cndx);
 void mdp4_dsi_video_wait4vsync(int cndx);
+void mdp4_mixer_reset(int mixer);
 void mdp4_dsi_cmd_pipe_queue(int cndx, struct mdp4_overlay_pipe *pipe);
 void mdp4_dsi_video_pipe_queue(int cndx, struct mdp4_overlay_pipe *pipe);
 void mdp4_dsi_cmd_vsync_ctrl(struct fb_info *info, int enable);
@@ -984,6 +985,7 @@ void mdp4_overlay_mdp_perf_upd(struct msm_fb_data_type *mfd, int flag);
 int mdp4_update_base_blend(struct msm_fb_data_type *mfd,
 				struct mdp_blend_cfg *mdp_blend_cfg);
 u32 mdp4_get_mixer_num(u32 panel_type);
+void mdp4_vg_csc_restore(void);
 
 #ifndef CONFIG_FB_MSM_WRITEBACK_MSM_PANEL
 static inline void mdp4_wfd_pipe_queue(int cndx, struct mdp4_overlay_pipe *pipe)
