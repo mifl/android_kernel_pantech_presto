e92e5cde4cd3ec6b23444b3a832610e97227c013
mifl <mifl.pc@gmail.com>
2014-11-04 17:16:44 +0100

    bcmdhd: presto: adapt changes into bcmdhd directory
    Change-Id: Ib6f20c2e5a3c700816d10f5f8b3fab220561cce2

--- drivers/net/wireless/bcmdhd/dhd_custom_gpio.c
+++ drivers/net/wireless/bcmdhd/dhd_custom_gpio.c
@@ -1,8 +1,8 @@
 /*
 * Customer code to add GPIO control during WLAN start/stop
-* Copyright (C) 1999-2011, Broadcom Corporation
+* Copyright (C) 1999-2012, Broadcom Corporation
 * 
-*         Unless you and Broadcom execute a separate written software license
+*      Unless you and Broadcom execute a separate written software license
 * agreement governing use of this software, this software is licensed to you
 * under the terms of the GNU General Public License version 2 (the "GPL"),
 * available at http://www.broadcom.com/licenses/GPLv2.php, with the
@@ -20,7 +20,7 @@
 * software in any way with any other Broadcom software provided under a license
 * other than the GPL, without Broadcom's express prior written consent.
 *
-* $Id: dhd_custom_gpio.c 339054 2012-06-15 04:56:55Z $
+* $Id: dhd_custom_gpio.c 353167 2012-08-24 22:11:30Z $
 */
 
 #include <typedefs.h>
@@ -34,14 +34,27 @@
 #include <wlioctl.h>
 #include <wl_iw.h>
 
+#ifdef CUSTOMER_HW_PT
+#include <asm/gpio.h>
+#include <linux/random.h>
+#endif
+
 #define WL_ERROR(x) printf x
 #define WL_TRACE(x)
 
+#ifdef CUSTOMER_HW_PT
+struct dhd_info;
+extern int _dhd_set_mac_address(struct dhd_info *dhd,
+	int ifidx, struct ether_addr *addr);
+#endif
+#if defined(CUSTOMER_HW_PT) && !defined(CONFIG_MACH_MAHIMAHI)
+static int dhd_wl_reset_gpio = CONFIG_BCM4329_WL_RESET_GPIO;
+#endif
 #ifdef CUSTOMER_HW
 extern  void bcm_wlan_power_off(int);
 extern  void bcm_wlan_power_on(int);
 #endif /* CUSTOMER_HW */
-#if defined(CUSTOMER_HW2)
+#if defined(CUSTOMER_HW2) || defined(CUSTOMER_HW4)
 #ifdef CONFIG_WIFI_CONTROL_FUNC
 int wifi_set_power(int on, unsigned long msec);
 int wifi_get_irq_number(unsigned long *irq_flags_ptr);
@@ -53,9 +66,9 @@ int wifi_get_irq_number(unsigned long *irq_flags_ptr) { return -1; }
 int wifi_get_mac_addr(unsigned char *buf) { return -1; }
 void *wifi_get_country_code(char *ccode) { return NULL; }
 #endif /* CONFIG_WIFI_CONTROL_FUNC */
-#endif /* CUSTOMER_HW2 */
+#endif /* CUSTOMER_HW2 || CUSTOMER_HW4 */
 
-#if defined(OOB_INTR_ONLY)
+#if defined(OOB_INTR_ONLY) || defined(BCMSPI_ANDROID)
 
 #if defined(BCMLXSDMMC)
 extern int sdioh_mmc_irq(int irq);
@@ -86,7 +99,7 @@ int dhd_customer_oob_irq_map(unsigned long *irq_flags_ptr)
 {
 	int  host_oob_irq = 0;
 
-#ifdef CUSTOMER_HW2
+#if defined(CUSTOMER_HW2) || defined(CUSTOMER_HW4)
 	host_oob_irq = wifi_get_irq_number(irq_flags_ptr);
 
 #else
@@ -94,29 +107,29 @@ int dhd_customer_oob_irq_map(unsigned long *irq_flags_ptr)
 	if (dhd_oob_gpio_num < 0) {
 		dhd_oob_gpio_num = CUSTOM_OOB_GPIO_NUM;
 	}
-#endif /* CUSTOMER_HW2 */
+#endif /* CUSTOMER_OOB_GPIO_NUM */
 
 	if (dhd_oob_gpio_num < 0) {
-		WL_ERROR(("%s: ERROR customer specific Host GPIO is NOT defined\n",
-			__FUNCTION__));
+		WL_ERROR(("%s: ERROR customer specific Host GPIO is NOT defined \n",
+		__FUNCTION__));
 		return (dhd_oob_gpio_num);
 	}
 
 	WL_ERROR(("%s: customer specific Host GPIO number is (%d)\n",
-		__FUNCTION__, dhd_oob_gpio_num));
+	         __FUNCTION__, dhd_oob_gpio_num));
 
-#if defined CUSTOMER_HW
+#if defined (CUSTOMER_HW) || defined(CUSTOMER_HW_PT)
 	host_oob_irq = MSM_GPIO_TO_INT(dhd_oob_gpio_num);
 #elif defined CUSTOMER_HW3
 	gpio_request(dhd_oob_gpio_num, "oob irq");
 	host_oob_irq = gpio_to_irq(dhd_oob_gpio_num);
 	gpio_direction_input(dhd_oob_gpio_num);
 #endif /* CUSTOMER_HW */
-#endif /* CUSTOMER_HW2 */
+#endif /* CUSTOMER_HW2 || CUSTOMER_HW4 */
 
 	return (host_oob_irq);
 }
-#endif /* defined(OOB_INTR_ONLY) */
+#endif /* defined(OOB_INTR_ONLY) || defined(BCMSPI_ANDROID) */
 
 /* Customer function to control hw specific wlan gpios */
 void
@@ -129,8 +142,12 @@ dhd_customer_gpio_wlan_ctrl(int onoff)
 #ifdef CUSTOMER_HW
 			bcm_wlan_power_off(2);
 #endif /* CUSTOMER_HW */
-#ifdef CUSTOMER_HW2
+#if defined(CUSTOMER_HW2) || defined(CUSTOMER_HW4)
 			wifi_set_power(0, 0);
+#elif defined(CUSTOMER_HW_PT)
+			gpio_set_value(dhd_wl_reset_gpio, 0);
+			printk("WLAN_RESET_OFF[%d]\n",
+				gpio_get_value(dhd_wl_reset_gpio));
 #endif
 			WL_ERROR(("=========== WLAN placed in RESET ========\n"));
 		break;
@@ -141,8 +158,12 @@ dhd_customer_gpio_wlan_ctrl(int onoff)
 #ifdef CUSTOMER_HW
 			bcm_wlan_power_on(2);
 #endif /* CUSTOMER_HW */
-#ifdef CUSTOMER_HW2
+#if defined(CUSTOMER_HW2) || defined(CUSTOMER_HW4)
 			wifi_set_power(1, 0);
+#elif defined(CUSTOMER_HW_PT)
+			gpio_set_value(dhd_wl_reset_gpio, 1);
+			WL_ERROR("WLAN_RESET_ON[%d]\n",
+				gpio_get_value(dhd_wl_reset_gpio));
 #endif
 			WL_ERROR(("=========== WLAN going back to live  ========\n"));
 		break;
@@ -153,6 +174,11 @@ dhd_customer_gpio_wlan_ctrl(int onoff)
 #ifdef CUSTOMER_HW
 			bcm_wlan_power_off(1);
 #endif /* CUSTOMER_HW */
+#if defined(CUSTOMER_HW_PT) && !defined(CONFIG_MACH_MAHIMAHI)
+			gpio_set_value(dhd_wl_reset_gpio, 0);
+			printk("WLAN_POWER_OFF[%d]\n",
+				gpio_get_value(dhd_wl_reset_gpio));
+#endif
 		break;
 
 		case WLAN_POWER_ON:
@@ -160,14 +186,83 @@ dhd_customer_gpio_wlan_ctrl(int onoff)
 				__FUNCTION__));
 #ifdef CUSTOMER_HW
 			bcm_wlan_power_on(1);
+#endif /* CUSTOMER_HW */
+#if defined(CUSTOMER_HW_PT) && !defined(CONFIG_MACH_MAHIMAHI)
+			gpio_set_value(dhd_wl_reset_gpio, 1);
+			printk("WLAN_POWER_ON[%d]\n",
+				gpio_get_value(dhd_wl_reset_gpio));
+#endif
 			/* Lets customer power to get stable */
 			OSL_DELAY(200);
-#endif /* CUSTOMER_HW */
 		break;
 	}
 }
 
 #ifdef GET_CUSTOM_MAC_ENABLE
+#ifdef CUSTOMER_HW_PT
+static int
+dhd_read_mac_from_file(char *fpath, struct ether_addr *addr)
+{
+	void *fp = NULL;
+	int len;
+	int ret = -1;
+	char ethbuf[32] = {0, };
+	struct ether_addr tmp;
+
+	if ( !fpath || !addr )
+		return -1;
+
+	if ((fp = dhd_os_open_image(fpath)) == NULL)
+		return -1;
+
+	len = dhd_os_get_image_block(ethbuf, 32, fp);
+	if ( len < 17 ) /* mac address format xx:xx:xx:xx:xx:xx */
+		goto err;
+
+	ethbuf[17] = '\0';
+	if (sscanf(ethbuf, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+			&tmp.octet[0], &tmp.octet[1], &tmp.octet[2],
+			&tmp.octet[3], &tmp.octet[4], &tmp.octet[5]) != 6)
+		goto err;
+
+	memcpy(addr, &tmp, sizeof(struct ether_addr));
+
+	ret = 0;
+err:
+	dhd_os_close_image(fp);
+	return ret;
+}
+
+static int
+dhd_write_mac_to_file(char *fpath, struct ether_addr *addr)
+{
+	struct file *fp = NULL;
+	char ethbuf[32] = {0, };
+	mm_segment_t old_fs;
+	loff_t pos;
+
+	if ( !fpath || !addr )
+		return -1;
+
+	fp = filp_open(fpath, O_CREAT | O_RDWR, 0666);
+	if (IS_ERR(fp))
+		return -1;
+
+	sprintf(ethbuf, "%02x:%02x:%02x:%02x:%02x:%02x",
+			addr->octet[0], addr->octet[1], addr->octet[2],
+			addr->octet[3], addr->octet[4], addr->octet[5]);
+
+	old_fs = get_fs();
+	set_fs(get_ds());
+	pos = fp->f_pos;
+	vfs_write(fp, ethbuf, 18, &pos);
+	set_fs(old_fs);
+
+	filp_close(fp, NULL);
+
+	return 0;
+}
+#endif
 /* Function to get custom MAC address */
 int
 dhd_custom_get_mac_address(unsigned char *buf)
@@ -189,12 +284,100 @@ dhd_custom_get_mac_address(unsigned char *buf)
 		struct ether_addr ea_example = {{0x00, 0x11, 0x22, 0x33, 0x44, 0xFF}};
 		bcopy((char *)&ea_example, buf, sizeof(struct ether_addr));
 	}
-#endif /* EXAMPLE_GET_MAC */
+#ifdef CUSTOMER_HW_PT
+	{
+		char panmac_path[] = "/dev/panmac";
+		static struct ether_addr mac;
+		static int custom_mac_applied = 0;
 
+		if ( custom_mac_applied )
+		{
+			memcpy(buf, &mac, sizeof(struct ether_addr));
+			return 0;
+		}
+
+		ret = dhd_read_mac_from_file(panmac_path, &mac);
+
+		if ( ret == 0 )
+		{
+			memcpy(buf, &mac, sizeof(struct ether_addr));
+			custom_mac_applied = 1;
+		}
+		else
+		{
+			int i;
+			char rand_panmac_path[] = "/data/misc/wifi/panmac";
+
+			ret = dhd_read_mac_from_file(rand_panmac_path, &mac);
+			if ( ret != 0 )
+			{
+				mac.octet[0] = 0x00;
+				mac.octet[1] = 0x0f;
+				mac.octet[2] = 0xe4;
+				for ( i = 3; i < ETHER_ADDR_LEN; i++ )
+					get_random_bytes(&mac.octet[i], 1);
+
+				dhd_write_mac_to_file(rand_panmac_path, &mac);
+			}
+			memcpy(buf, &mac, sizeof(struct ether_addr));
+			custom_mac_applied = 1;
+			ret = 0;
+		}
+	}
+#endif
+#endif /* EXAMPLE_GET_MAC */
 	return ret;
 }
 #endif /* GET_CUSTOM_MAC_ENABLE */
 
+#ifdef CUSTOMER_HW_PT
+int get_mac_address(struct dhd_info *dhd)
+{
+		int ret = 0;
+		char buf[18]      = {0};
+		char panmac_path[] = "/dev/panmac";
+		static struct ether_addr mac;
+		static int custom_mac_applied = 0;
+
+		if ( custom_mac_applied )
+		{
+			memcpy(buf, &mac, sizeof(struct ether_addr));
+			return 0;
+		}
+
+		ret = dhd_read_mac_from_file(panmac_path, &mac);
+
+		if ( ret == 0 )
+		{
+			memcpy(buf, &mac, sizeof(struct ether_addr));
+			custom_mac_applied = 1;
+		}
+		else
+		{
+			int i;
+			char rand_panmac_path[] = "/data/misc/wifi/panmac";
+
+			ret = dhd_read_mac_from_file(rand_panmac_path, &mac);
+			if ( ret != 0 )
+			{
+				mac.octet[0] = 0x00;
+				mac.octet[1] = 0x0f;
+				mac.octet[2] = 0xe4;
+				for ( i = 3; i < ETHER_ADDR_LEN; i++ )
+					get_random_bytes(&mac.octet[i], 1);
+
+				dhd_write_mac_to_file(rand_panmac_path, &mac);
+			}
+			memcpy(buf, &mac, sizeof(struct ether_addr));
+			_dhd_set_mac_address(dhd, 0, &mac);
+			custom_mac_applied = 1;
+			ret = 0;
+		}
+  return ret;
+}  
+#endif
+
+#ifndef CUSTOMER_HW4
 /* Customized Locale table : OPTIONAL feature */
 const struct cntry_locales_custom translate_custom_table[] = {
 /* Table should be filled out based on custom platform regulatory requirement */
@@ -289,5 +472,6 @@ void get_customized_country_code(char *country_iso_code, wl_country_t *cspec)
 	cspec->rev = translate_custom_table[0].custom_locale_rev;
 #endif /* EXMAPLE_TABLE */
 	return;
-#endif /* defined(CUSTOMER_HW2) && (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39)) */
+#endif /* defined(CUSTOMER_HW2) && (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 36)) */
 }
+#endif /* CUSTOMER_HW4 */
