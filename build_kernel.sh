5a79a2eb7a8927c15221e9660aa5fc1270589826
mifl <mifl.pc@gmail.com>
2014-11-14 18:30:24 +0100

    Kernel script: Change Toolchain to "arm-eabi-4.7".
    This is the default toolchain that is used in cm-11.0.
    
    Change-Id: Iaf85df39f08b5e5d1d98fda00e16c724b0e23d21

--- build_kernel.sh
+++ build_kernel.sh
@@ -11,7 +11,7 @@
 # set toolchain
 ##############################################################################
 export ARCH=arm
-export CROSS_COMPILE=$PWD/../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+export CROSS_COMPILE=$PWD/../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
 export LINUX_BIN_PATH=$PWD/obj
 rm -rf $LINUX_BIN_PATH
 CMD_V_LOG_FILE=$PWD/KERNEL_build.log
